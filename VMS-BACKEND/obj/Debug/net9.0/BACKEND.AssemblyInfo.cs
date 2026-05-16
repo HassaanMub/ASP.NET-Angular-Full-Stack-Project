4497f6273ab8c74f2c6b90050e1b4f829ff8ed7c
HassaanMub <hassaanmubark99@gmail.com>
2026-05-17 02:46:12 +0500

    homepage modified

--- VMS-BACKEND/obj/Debug/net9.0/BACKEND.AssemblyInfo.cs
+++ VMS-BACKEND/obj/Debug/net9.0/BACKEND.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BACKEND")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83eaf22ace7de2006694fe55d0fe87130fbf76f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("BACKEND")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BACKEND")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- VMS-FRONTEND/src/app/pages/home/home.ts
+++ VMS-FRONTEND/src/app/pages/home/home.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy, ChangeDetectorRef } from '@angular/core'; // ← Added ChangeDetectorRef
 
 @Component({
   selector: 'app-home',
@@ -6,4 +6,55 @@ import { Component } from '@angular/core';
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {}
+export class Home implements OnInit, OnDestroy {
+  public displayedText: string = '';
+  
+  private fullText: string = 'Your Next Car Starts Here.';
+  private currentIdx: number = 0;
+  private typingSpeed: number = 75;
+  private pauseDuration: number = 2500;
+  private timeoutId: any;
+
+  // Inject ChangeDetectorRef into the constructor
+  constructor(private cdr: ChangeDetectorRef) {}
+
+  ngOnInit(): void {
+    this.typeEffect();
+  }
+
+  private typeEffect(): void {
+    if (this.currentIdx < this.fullText.length) {
+      this.displayedText += this.fullText.charAt(this.currentIdx);
+      this.currentIdx++;
+      this.cdr.detectChanges(); 
+      
+      this.timeoutId = setTimeout(() => this.typeEffect(), this.typingSpeed);
+    } 
+    else {
+      this.timeoutId = setTimeout(() => {
+        this.displayedText = '';
+        this.currentIdx = 0;
+        this.cdr.detectChanges(); 
+        this.typeEffect();
+      }, this.pauseDuration);
+    }
+  }
+
+  ngOnDestroy(): void {
+    if (this.timeoutId) {
+      clearTimeout(this.timeoutId);
+    }
+  }
+}
+
+
+
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-home',
+//   imports: [],
+//   templateUrl: './home.html',
+//   styleUrl: './home.css',
+// })
+// export class Home {}
